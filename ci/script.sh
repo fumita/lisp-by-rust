bfa09ee540eecfded6b0accbc5dca6cf5e76551d
maikiichan <maikiilion@gmail.com>
2019-07-21 02:18:55 +0900

    Add shebang

--- ci/script.sh
+++ ci/script.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # This script takes care of testing your crate
 
 set -ex
@@ -13,9 +14,6 @@ main() {
 
     cross test --target $TARGET
     cross test --target $TARGET --release
-
-    cross run --target $TARGET
-    cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
